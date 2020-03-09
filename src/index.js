aeb734a4cd26bd377252731358f03bc0037abac9
j-Myles <42116741+j-Myles@users.noreply.github.com>
2020-03-09 18:32:57 -0400

    Create basic camera

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ function init() {
 
 }
 
+function set_camera() {
+    camera = new THREE.PerspectiveCamera(50, window.innerWidth, window.innerHeight,
+        0.1, 1000);
+}
+
 function set_scene() {
     scene = new Scene();
 }
